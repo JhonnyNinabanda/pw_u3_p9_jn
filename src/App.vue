ebee1b6727e538485a3a619c84a133c2d01fbaf1
JhonnyNinabanda <jeninabandap@uce.edu.ec>
2026-01-15 22:58:59 -0500

    Correccion del codigo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    |<router-link to=" `/hola-mundo/${idEstudiante}` ">Hola Mundo</router-link> |
+    |<router-link :to=" `/hola-mundo/${idEstudiante}` ">Hola Mundo</router-link> |
     | <router-link to="/pregunta">Pregunta</router-link>
     | <router-link to="/pokemon">Pokemon</router-link>
     
